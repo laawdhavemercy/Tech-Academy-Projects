6fc7ae3366d84f7c40d369f8344e31100957611e
laawdhavemercy <joshua,bar2lome@gmail.com>
2020-07-13 13:32:45 -0700

    uploaded
    pg 86

---
+++ Javascript/JS/basic_javascript_3.js
@@ -0,0 +1,49 @@
+function addition_Function() {
+	var addition = 5 + 5;
+	document.getElementById("Math").innerHTML = "5 + 5 = " + addition;
+}
+
+function subtraction_Function() {
+	var Subtraction = 5 - 2;
+	document.getElementById("Math").innerHTML = "5 - 2 = " + Subtraction;
+}
+
+function multiplication() {
+	var simple_Math = 6 * 8;
+	document.getElementById("Math").innerHTML = "6 x 8 = " + simple_Math;
+}
+
+function division () {
+	var simple_Math = 48 / 6;
+	document.getElementById("Math").innerHTML = "48 / 6 = " + simple_Math;
+}
+
+function more_Math() {
+	var simple_Math = (1 + 2) * 10 / 2 - 5;
+	document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
+}
+
+function modulus_Operator() {
+	var simple_Math = 25 % 6;
+	document.getElementById("Math").innerHTML = "When you divide 25 into 6 you have a remainder of: " + simple_Math;
+}
+
+function negation_Operator() {
+	var x = 10;
+	document.getElementById("math").innerHTML = -x;
+}
+
+var D = 5;
+D++;
+document.write(D);
+
+var J = 5.25;
+J--;
+document.write(J);
+
+window.alert(Math.random());
+
+window.alert(Math.random() * 100;
+
+Math.sqrt(49);
+
